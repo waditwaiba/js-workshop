e3a88ac28e6511c4fd5a8fe4f542ad59ad7aeb6e
faiyazbits <m.faiyaz123@gmail.com>
2024-08-31 17:30:05 +0530

    refactored existing tests

--- repeatString/index.js
+++ repeatString/index.js
@@ -1,13 +1,6 @@
-const repeatString = function () {
+const repeatString = function (word, times) {
 
 };
 
-// Do not edit below this line
 module.exports = repeatString;
 
-/* if (times < 0) return "ERROR";
-let string = "";
-for (let i = 0; i < times; i++) {
-    string += word;
-}
-return string; */
--- repeatString/index.test.js
+++ repeatString/index.test.js
@@ -4,19 +4,19 @@ describe('repeatString', () => {
     test('repeats the string', () => {
         expect(repeatString('hey', 3)).toEqual('heyheyhey');
     });
-    test.skip('repeats the string many times', () => {
+    test('repeats the string many times', () => {
         expect(repeatString('hello', 10)).toEqual('hellohellohellohellohellohellohellohellohellohello');
     });
-    test.skip('repeats the string 1 times', () => {
+    test('repeats the string 1 times', () => {
         expect(repeatString('hi', 1)).toEqual('hi');
     });
-    test.skip('repeats the string 0 times', () => {
+    test('repeats the string 0 times', () => {
         expect(repeatString('bye', 0)).toEqual('');
     });
-    test.skip('returns ERROR with negative numbers', () => {
+    test('returns ERROR with negative numbers', () => {
         expect(repeatString('goodbye', -1)).toEqual('ERROR');
     });
-    test.skip('works with blank strings', () => {
+    test('works with blank strings', () => {
         expect(repeatString('', 10)).toEqual('');
     });
 });
