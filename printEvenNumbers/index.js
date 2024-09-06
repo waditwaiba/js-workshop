06e53b76ebc459e52dfe4ff525f53e68d3c828d7
AishaSherin <aishaatwaiba@gmail.com>
2024-09-06 16:35:30 +0530

    printevennumber

--- printEvenNumbers/index.js
+++ printEvenNumbers/index.js
@@ -3,7 +3,11 @@
   * Expected Result: (15) => 0,2,4,6,8,10,12,14
   * Task Complexity: 1 of 5
 */
-const printEvenNumbers = (size) => {
+const printEvenNumbers = function (size) {
+  for (let i = 0; i <= Array.length; i++) {
+    i += 2, num % 2 == 0;
+  }
+  return size;
 }
 
 module.exports = printEvenNumbers
