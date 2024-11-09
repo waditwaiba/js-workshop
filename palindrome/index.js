d123ae67d29960e35692952a069068884fbbd3f3
swabah <ahmedswabah922@gmail.com>
2024-11-09 17:51:39 +0530

    palindrome completed

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (pal) {
+    let er = /[^A-Za-z-0-9]/g;
+    pal = pal.replace(er, "").toLowerCase();
+    let Len = pal.length;
+    for (let i = 0; i < Len / 2; i++) {
+        if (pal[i] !== pal[[Len -1-i]]) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
