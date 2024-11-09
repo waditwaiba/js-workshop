84159bf4df96c17a3a418e71750162b0260989e0
swabah <ahmedswabah922@gmail.com>
2024-11-09 17:51:15 +0530

    reverseString completed

--- reverseString/index.js
+++ reverseString/index.js
@@ -1,5 +1,11 @@
-const reverseString = function () {
+/**
+ * Reverses a given string.
+ * @param {string} str - The string to reverse
+ * @returns {string} The reversed string
+ */
 
+const reverseString = function (string) {
+    return string.split("").reverse().join("");  
 };
 
 // Do not edit below this line
