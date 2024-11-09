e274e45a161b4bc4aae4a4a6b94fd5c91840a252
swabah <ahmedswabah922@gmail.com>
2024-11-09 17:52:08 +0530

    repeatString completed

--- repeatString/index.js
+++ repeatString/index.js
@@ -1,5 +1,12 @@
+/**
+ * Repeats a given string a specified number of times.
+ * 
+ * @param {string} word - The string to be repeated.
+ * @param {number} times - The number of times to repeat the string.
+ * @returns {string} - The resulting string after repetition.
+ */
 const repeatString = function (word, times) {
-
+  return word.repeat(times);
 };
 
 module.exports = repeatString;
