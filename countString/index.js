ea5b3726895bc82e1b6ff1ed9a96b99b9470bc7c
swabah <ahmedswabah922@gmail.com>
2024-12-10 11:16:28 +0530

    count string completed

--- countString/index.js
+++ countString/index.js
@@ -1,9 +1,12 @@
 /**
   * Task description: Write a method to count the occurrence of a substring in a string
-  * Expected Result: (The quick brown fox jumps over the lazy dog) => 2
+  * Expected Result: ("The quick brown fox jumps over the lazy dog", "The") => 2
   * Task Complexity: 1 of 5
 */
+
 const count = (str, wordToFind) => {
+  const words = str.toLowerCase().split(" ")
+  return words.filter(word => word === wordToFind.toLowerCase()).length
 }
 
 module.exports = count;
